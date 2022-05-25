80bc6434abc8d1acc3c6bb8ad587f669ae28018c
unknownMusician <unknown.musician.t@gmail.com>
2022-05-25 23:56:25 +0300

    Add changePriceForAllTickets and changeTicketStatus

--- src/main/java/com/example/lab2cinema/model/TicketStatus.java
+++ src/main/java/com/example/lab2cinema/model/TicketStatus.java
@@ -1,5 +1,13 @@
 package com.example.lab2cinema.model;
 
 public enum TicketStatus {
-    NotBought,Bought
+    NotBought,Bought;
+
+    public int ToInt() {
+        switch (this) {
+            case NotBought: return 0;
+            case Bought: return 1;
+            default: throw new IllegalArgumentException();
+        }
+    }
 }
--- src/main/java/com/example/lab2cinema/repo/TicketRepoImpl.java
+++ src/main/java/com/example/lab2cinema/repo/TicketRepoImpl.java
@@ -16,6 +16,7 @@ public class TicketRepoImpl implements TicketRepo {
 
     private static final String SELECT_BY_SEANCE_ID = "SELECT * FROM ticket WHERE seanceId = ?;";
     private static final String UPDATE_PRICE = "UPDATE ticket SET price = ? WHERE seanceId = ?;";
+    private static final String UPDATE_STATUS = "UPDATE ticket SET ticket_status = ? WHERE seanceId = ? AND number = ?;";
 
     public SeanceRepoImpl(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
@@ -24,11 +25,11 @@ public class TicketRepoImpl implements TicketRepo {
     @Override
     public List<Ticket> findTicketsBySeanceId(Integer seanceId) {
         try {
-             return jdbcTemplate.query((Connection c) -> {
+            return jdbcTemplate.query((Connection c) -> {
                 PreparedStatement preparedStatement = c.prepareStatement(SELECT_BY_SEANCE_ID);
                 preparedStatement.setInt(1, seanceId);
                 return preparedStatement;
-            },seanceRowMapper).get(0);
+            },seanceRowMapper);
 
         }
         catch (EmptyResultDataAccessException e) {
@@ -39,13 +40,20 @@ public class TicketRepoImpl implements TicketRepo {
 
     @Override
     public void generateTickets(int seanceId, int amountOfTickets) {
-        try {
-             return jdbcTemplate.query((Connection c) -> {
-                PreparedStatement preparedStatement = c.prepareStatement(SELECT_BY_SEANCE_ID);
-                preparedStatement.setInt(1, seanceId);
-                return preparedStatement;
-            },seanceRowMapper).get(0);
 
+    }
+
+    @Override
+    public List<Ticket> changePriceForAllTickets(int seanceId, int newPrice) {
+        try {
+            jdbcTemplate.query((Connection c) -> {
+                PreparedStatement preparedStatement = c.prepareStatement(UPDATE_PRICE);
+                preparedStatement.setInt(1, newPrice);
+                preparedStatement.setInt(2, seanceId);
+                return preparedStatement;
+            },seanceRowMapper);
+
+            return findTicketsBySeanceId(seanceId);
         }
         catch (EmptyResultDataAccessException e) {
             System.out.println("Fuck you");
@@ -54,13 +62,20 @@ public class TicketRepoImpl implements TicketRepo {
     }
 
     @Override
-    public List<Ticket> changePriceForAllTickets(int seanceId, int newPrice) {
-        return null;
-    }
-
-    @Override
     public Ticket changeTicketStatus(Integer seanceId, Integer ticketNumber, TicketStatus ticketStatus) {
-        return null;
+        try {
+            jdbcTemplate.query((Connection c) -> {
+                PreparedStatement preparedStatement = c.prepareStatement(UPDATE_STATUS);
+                preparedStatement.setInt(1, ticketStatus.ToInt());
+                preparedStatement.setInt(2, seanceId);
+                preparedStatement.setInt(3, ticketNumber);
+                return preparedStatement;
+            },seanceRowMapper);
+        }
+        catch (EmptyResultDataAccessException e) {
+            System.out.println("Fuck you");
+            return;
+        }
     }
 
     @Override
